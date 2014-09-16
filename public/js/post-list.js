4db5bdb7730d0385a2c6da96fa8f4aef61f47376
Vikram Tiwari <Vikramtheone1@gmail.com>
2014-09-16 22:37:18 +0530

    Remove Modularization
    Unfortunately have to remove modularization. Caused errors. Need to
    study more.
    
    PS: Damn cache! Couldn't even tell the issue in previous build. -1:

--- public/js/post-list.js
+++
@@ -1,6 +0,0 @@
-Polymer({
-    handleFavorite: function(event, detail, sender) {
-        var post = sender.templateInstance.model.post;
-        this.$.service.setFavorite(post.uid, post.favorite);
-    }
-});
