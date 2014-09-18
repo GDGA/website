7319855db95af64a362477269b5188bbae30b33b
Vikram Tiwari <Vikramtheone1@gmail.com>
2014-09-18 11:48:46 +0530

    Adding Map to a new tab - 1
    Map added to all the tabs :-1:

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 var tabs = document.querySelector('paper-tabs');
 var list = document.querySelector('post-list');
+var map = document.querySelector('google-map');
 
 tabs.addEventListener('core-select', function() {
     list.show = tabs.selected;
